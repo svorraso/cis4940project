d3f0cef5fd25bd08015005453223d1f52c9f3978
svorraso <113285520+svorraso@users.noreply.github.com>
2022-10-07 15:26:39 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
