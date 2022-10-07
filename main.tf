ca9ef4c997c04ca98bda7b5cdeeaca0bd45e2f5e
svorraso <113285520+svorraso@users.noreply.github.com>
2022-10-07 16:03:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,8 +84,8 @@ resource "azurerm_linux_virtual_machine" "main" {
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
   size                            = "Standard_D2s_v3"
-  admin_username                  = "${var.username}"
-  admin_password                  = "${var.password}"
+  admin_username                  = "userlogin"
+  admin_password                  = "Thisismypassword123%"
   disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.main.id,
